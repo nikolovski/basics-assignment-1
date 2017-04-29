d2b64e194e2389f2d07f5ac75fe03588992d5c68
Martino Nikolovski <nikolovski23@gmail.com>
2017-04-29 19:00:03 -0400

    Initial commit

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  template: `<div class="alert alert-success">This is the success component!</div>`
+})
+
+export class SuccessAlertComponent { }
